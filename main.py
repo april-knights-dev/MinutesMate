9b053d62986a079cb5774d692a5340f9506a9765
shngmsw <mose1021@gmail.com>
2023-07-03 17:37:34 +0900

    Refactor code to calculate system template token count from the text and split the text accordingly. Clean up code structure and remove unnecessary print statements. Improve message formatting and handle thread replies more efficiently.

--- main.py
+++ main.py
@@ -67,43 +67,53 @@ def handle_shortcut(ack, body, logger):
 
                 print(output)
                 # 途中経過をスレッドに投稿する
-                client.chat_postMessage(channel=channel, text="書き起こしが終わりました。もう少し待ってね", thread_ts=message_id)
+                upload_to_slack(channel, output, filepath, "書き起こしが終わりました。要約はもう少し待ってね", message_id)
 
                 # chatgpt apiを使ってサマリーする
                 system_template = """会議の書き起こしが渡されます。
-                この会議のサマリーをMarkdown形式で作成してください。サマリーは、以下のような形式で書いてください。
+                この会議のサマリーをMarkdown形式で作成してください。
+                サマリーは、以下のような形式で書いてください。
 
                 - 会議の目的
                 - 会議の内容
                 - 会議の結果
                 - 次回の会議までのタスク
+
                 """
+                user_first_template = """これから文章を渡すので、その内容を要約してください。ただし文章は分割してあるので「作業してください」と伝えるまで、あなたは作業を始めず、代わりに「次の入力を待っています」と回答してください。"""
                 full_text = transcript.text  # テキスト全体を取得
-                # system_tempalteのトークンを計算
+                # system_templateのトークンをテキストから計算
                 system_template_token_count = len(system_template.split())
                 # system_template_token_countを足して10000になるように分割
-                segments = [full_text[i:i+10000-system_template_token_count]
-                            for i in range(0, len(full_text), 10000-system_template_token_count)]
+                split_count = 10000 - system_template_token_count
+                segments = [full_text[i:i+split_count]
+                            for i in range(0, len(full_text), split_count)]
                 messages = []
                 for i, segment in enumerate(segments):
                     if i == 0:
                         messages.append({"role": "system", "content": system_template})
+                        messages.append({"role": "user", "content": user_first_template})
+
                     messages.append({"role": "user", "content": segment})
 
+                    if i == len(segments):
+                        messages.append({"role": "user", "content": "作業してください"})
+                    else:
+                        messages.append({"role": "assistant", "content": "次の入力を待っています"})
+
                 final_text = ""
-                for message in messages:
-                    response = openai.ChatCompletion.create(
-                        model="gpt-3.5-turbo-16k-0613",
-                        messages=[message],
-                        max_tokens=16000,
-                        temperature=0.1
-                    )
-                    generated_text = response['choices'][0]['message']['content']
-                    final_text += generated_text
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo-16k-0613",
+                    messages=messages,
+                    max_tokens=16000,
+                    temperature=0.1
+                )
+                generated_text = response['choices'][0]['message']['content']
+                final_text += generated_text
 
                 print(final_text)
 
-                upload_to_slack(channel, full_text, filepath, final_text, message_id)
+                upload_to_slack(channel, final_text, "", final_text, message_id)
 
             else:
                 return
